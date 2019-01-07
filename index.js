cdcd67473d47285f961122411298c70ff0bcbb6b
Peter Yost <yostpa@gmail.com>
2019-01-07 17:00:15 +0100

    the rest

---
+++ index.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+    name: 'opti-grid',
+
+    isDevelopingAddon() {
+        return true;
+    },
+
+    included(app, parentAddon) {
+        this._super.included.apply(this, arguments);
+
+        let target = (parentAddon || app);
+        target.options = target.options || {}; // Ensures options exists for Scss/Less below
+
+        target.import('vendor/opti-grid-styles.css');
+        target.import('vendor/tableHeadFixer.js');
+        target.import('vendor/colResizable-1.6.js');
+
+    },
+};
